51739991471d19f1e0eb08db6184db2e2c6552fd
Pirunpoika <kirkkonen@gmail.com>
2019-08-25 21:09:19 +0300

    Basic UI done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { TransactionDetailsComponent } from './transaction-details/transaction-details.component';
+import { BlockSummaryComponent } from './block-summary/block-summary.component';
+import { BlockHistoryComponent } from './block-history/block-history.component';
+import { AddressComponent } from './address/address.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BlockHistoryComponent },
+  { path: 'blocks', component: BlockSummaryComponent},
+  { path: 'transactions', component: TransactionDetailsComponent },
+  { path: 'addresses', component: AddressComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockSummaryComponent } from './block-summary/block-summary.component';
+import { TransactionDetailsComponent } from './transaction-details/transaction-details.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { BlockHistoryComponent } from './block-history/block-history.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockSummaryComponent,
+    TransactionDetailsComponent,
+    HeaderComponent,
+    FooterComponent,
+    BlockHistoryComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
